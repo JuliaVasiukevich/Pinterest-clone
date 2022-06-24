60d89661129039dc125a13da353eecee699c9e07
JuliaVasiukevich <julliavas@yandex.ru>
2022-06-24 23:22:59 +0300

    add visibility modal window

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 
-import { desks } from "./components/desk/desk.js";
+// import { desks } from "./components/desk/desk.js";
+import { makeCards } from "./components/basic-card/basic-card.js";
+
+import { generateModalDesk } from "./components/modal-windows/modal-windows";
 
 // fetch('http://localhost:3000/desks').then(res => res.json()).then((res) => console.log(res))
 
 const desks = ["desk1", "desk2", "desk3"];
 const claims = ["test1", "test2", "test3", "test4", "test5", "test6"];
 
-import { makeCards } from "./components/basic-card/basic-card.js";
-
-// fetch('http://localhost:3000/desks').then(res => res.json()).then((res) => {
-//     const url = res.data[0].pictures[0].url;
-//     renderImage(url);
-// });
-
-// function renderImage(imageName) {
-//     const wrapper = document.getElementById('pep');
-//     const img = document.createElement('img');
-//     img.src = imageName;
-//     wrapper.append(img);
-// }
