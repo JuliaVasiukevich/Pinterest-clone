70500ddd17be743cb522e728c9f6df323fee2302
Julia Kovalchuk <yulya_kovalchuk_95@mail.ru>
2022-06-26 20:27:22 +0300

    resolve merge conflicts

--- src/components/modal-windows/modal-windows.js
+++ src/components/modal-windows/modal-windows.js
@@ -1,33 +1,39 @@
-import { make } from "../../utils.js";
-import { getDesks } from "../desk/desk.js";
-import { claims } from "../../main.js";
-import { makeCards } from "../basic-card/basic-card.js";
+import {
+  make
+} from "../../utils.js";
+import {
+  getDesks
+} from "../desk/desk.js";
+import {
+  makeCards
+} from "../basic-card/basic-card.js";
 
-export function appearModalWindows() {
-  document.body.addEventListener("click", (event) => {
-    if (
-      event.target.classList.contains("card__claim") ||
-      event.target.classList.contains("card__desk")
-    ) {
-      const modalWrapper = make("div", "modal-wrapper");
-      document.body.append(modalWrapper);
+export const claims = ["test1", "test2", "test3", "test4", "test5", "test6"];
 
-      const modalBody = make("div", "modal-body");
-      modalWrapper.append(modalBody);
+document.body.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("card__claim") ||
+    event.target.classList.contains("card__desk")
+  ) {
+    const modalWrapper = make("div", "modal-wrapper");
+    document.body.append(modalWrapper);
 
-      const modalWindow = make("div", "modal-window");
-      modalBody.append(modalWindow);
+    const modalBody = make("div", "modal-body");
+    modalWrapper.append(modalBody);
 
-      if (event.target.classList.contains("card__claim")) {
-        generateModalСlaims(claims);
-      }
+    const modalWindow = make("div", "modal-window");
+    modalBody.append(modalWindow);
 
-      if (event.target.classList.contains("card__desk")) {
-        getDesks(generateModalDesk);
-      }
+    if (event.target.classList.contains("card__claim")) {
+      generateModalСlaims(claims);
     }
-  });
-}
+
+    if (event.target.classList.contains("card__desk")) {
+      getDesks(generateModalDesk);
+    }
+  }
+});
+
 
 function generateModalDesk(desksArray) {
   const modalWindow = document.querySelector(".modal-window");
@@ -43,6 +49,8 @@ function generateModalDesk(desksArray) {
   }
   return;
 }
+
+// getDesks(generateModalDesk);
 
 function generateModalСlaims(claimsArray) {
   const modalWindow = document.querySelector(".modal-window");
@@ -111,25 +119,24 @@ method может быть либо GET, либо POST и определяет, 
   return;
 }
 
-appearModalWindows();
 
-export function disappearModalWindows() {
-  document.body.addEventListener("click", (event) => {
-    if (
-      event.target.classList.contains("modal-wrapper") ||
-      event.target.classList.contains("modal-close") ||
-      event.target.classList.contains("modal-body")
-    ) {
-      const modalWrapper = document.querySelector(".modal-wrapper");
-      modalWrapper.remove();
-    }
-  });
-}
+document.body.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("modal-wrapper") ||
+    event.target.classList.contains("modal-close") ||
+    event.target.classList.contains("modal-body")
+  ) {
+    const modalWrapper = document.querySelector(".modal-wrapper");
+    modalWrapper.remove();
+  }
+});
 
-disappearModalWindows();
+
+
 
 // function switchByDesk() {
 //   const deskElement = document.querySelectorAll(".modal-window__element");
 //   console.log(deskElement);
 // }
 // switchByDesk();
+// generateModalСlaims(claims);
