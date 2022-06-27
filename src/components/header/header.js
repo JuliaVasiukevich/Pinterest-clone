e273268f1fb0a3a847de7cf87e28ceb6f40ca96f
JuliaVasiukevich <julliavas@yandex.ru>
2022-06-27 20:33:04 +0300

    add pictures and local storage

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,31 @@
 export function header() {
 
-    const theme = document.body.querySelector('.theme');
-    console.log(theme)
+    const theme = document.body.querySelector('.theme__checkbox');
     theme.addEventListener("click", () => {
-        const html = document.getElementById('pop');
-        console.log(html);
-        html.classList.toggle("theme-dark")
-        html.classList.add('theme-light');
-        save();
+        themed()
     });
-    
-    const checked = JSON.parse(localStorage.getItem('theme'));
-    if (checked == true) {
-        document.getElementById("theme").checked = true;
+
+    function save() {
+        localStorage.setItem("theme", theme.checked);
     }
 
-    function load() {
-        theme.checked = checked;
-    }
-
-    function save(){
-        localStorage.setItem('theme', checkbox.checked);
-    }
-
-    load();
+    let checked = JSON.parse(localStorage.getItem("theme"));
+    theme.checked = checked;
+    window.addEventListener('change', save);
 }
 
 header();
+themed();
+
+function themed() {
+    const theme = document.body.querySelector('.theme__checkbox');
+    const html = document.getElementById('pop');
+    if (theme.checked) {
+        // html.classList.toggle("theme-light");
+        html.classList.add('theme-dark');
+        html.classList.remove('theme-light');
+    } else {
+        html.classList.add('theme-light');
+        html.classList.remove('theme-dark');
+    }
+};
