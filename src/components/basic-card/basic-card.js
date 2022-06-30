ae95dc62ce1b551191cdfbbca51f499d5143bed9
Anna <anichkayu@yandex.by>
2022-06-30 10:33:20 +0300

    fix: bug fixes update

--- src/components/basic-card/basic-card.js
+++ src/components/basic-card/basic-card.js
@@ -64,9 +64,6 @@ const cardsInfo = {
 };
 
 export function makeCards(data) {
-  // const cards = make("section", ["cards", "grid"]);
-  // document.body.append(cards);
-
   const cards = document.querySelector(".grid");
 
   for (let item of data["pictures"]) {
